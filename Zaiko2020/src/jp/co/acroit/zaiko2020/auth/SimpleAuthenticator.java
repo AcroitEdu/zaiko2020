f0a48e0e5c9985c4170c826cb9571ac2dcbaefd2
k-oishi <k-oishi@DESKTOP-GDLQN7T>
2020-05-15 09:44:09 +0900

    Implemented SimpleAuthenticator

---
+++ Zaiko2020/src/jp/co/acroit/zaiko2020/auth/SimpleAuthenticator.java
@@ -0,0 +1,31 @@
+package jp.co.acroit.zaiko2020.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import jp.co.acroit.zaiko2020.data.IUserDataAccess;
+import jp.co.acroit.zaiko2020.user.ISessionContainer;
+import jp.co.acroit.zaiko2020.user.ISessionContainerFactory;
+import jp.co.acroit.zaiko2020.user.IUser;
+
+public class SimpleAuthenticator implements IAuthenticator {
+    @Autowired
+    PasswordEncoder encoder;
+    @Autowired
+    IUserDataAccess users;
+    @Autowired
+    ISessionContainerFactory sessionFactory;
+    @Override
+    public ISessionContainer authenticate(String id, String password) throws Exception {
+        IUser user = users.findById(id);
+        if(user == null) {
+            throw new UserNotFoundException(String.format("User %s not found!", id));
+        }
+        if(user.authenticate(encoder, password)) {
+            return sessionFactory.create(user);
+        }else {
+            throw new AuthenticationException("Authentication failed");
+        }
+    }
+
+}
