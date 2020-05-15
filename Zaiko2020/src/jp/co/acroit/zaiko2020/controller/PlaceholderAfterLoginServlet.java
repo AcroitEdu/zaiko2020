0e5ab330b17f41baed107321344d0cb0ecf52e2c
k-oishi <k-oishi@DESKTOP-GDLQN7T>
2020-05-15 10:53:02 +0900

    Fixed PlaceholderAfterLoginServlet

--- Zaiko2020/src/jp/co/acroit/zaiko2020/controller/PlaceholderAfterLoginServlet.java
+++ Zaiko2020/src/jp/co/acroit/zaiko2020/controller/PlaceholderAfterLoginServlet.java
@@ -28,8 +28,7 @@ public final class PlaceholderAfterLoginServlet extends AutowireServletBase {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        //response.getWriter().append("Served at: ").append(request.getContextPath());
+        response.getWriter().append("Served at: ").append(request.getContextPath());
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/PlaceholderAfterLogin.jsp");
         dispatcher.forward(request, response);
     }
