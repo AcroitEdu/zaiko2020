04ae1d6fea07e49f0c3643718cd79d14dd6b1c03
k-oishi <k-oishi@DESKTOP-GDLQN7T>
2020-06-05 09:00:39 +0900

    補足

--- Zaiko2020/src/jp/co/acroit/zaiko2020/auth/PasswordComparator.java
+++ Zaiko2020/src/jp/co/acroit/zaiko2020/auth/PasswordComparator.java
@@ -2,13 +2,15 @@ package jp.co.acroit.zaiko2020.auth;
 
 public class PasswordComparator {
 
-	// ログインフォームで入力されたパスワードを、実際のパスワードと照合する
-	public static boolean comparator(String formPass, String pass) {
-		if(formPass.equals(pass)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    //TODO: メソッド名 comparator->compare
+    //TODO: staticを取る
+    // ログインフォームで入力されたパスワードを、実際のパスワードと照合する
+    public static boolean comparator(String formPass, String pass) {
+        if(formPass.equals(pass)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
