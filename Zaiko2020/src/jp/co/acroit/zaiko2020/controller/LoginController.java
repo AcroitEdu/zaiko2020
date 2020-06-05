f8f1383ad8fa24f31de021d2d64bffe07d311858
h-tajima <h-tajima@DESKTOP-06LSGLM>
2020-06-05 10:02:43 +0900

    修正

--- Zaiko2020/src/jp/co/acroit/zaiko2020/controller/LoginController.java
+++ Zaiko2020/src/jp/co/acroit/zaiko2020/controller/LoginController.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import jp.co.acroit.zaiko2020.user.User;
+
 /**
  * Servlet implementation class LoginController
  */
