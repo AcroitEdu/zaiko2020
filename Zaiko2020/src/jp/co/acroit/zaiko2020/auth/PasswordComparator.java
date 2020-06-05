c54d0008034a22ce9b3469bca748c47c86d06efc
h-ishioka <h-ishioka@DESKTOP-GD4RHQN>
2020-06-05 09:40:03 +0900

    PasswordComparator内のメソッド名を変更

--- Zaiko2020/src/jp/co/acroit/zaiko2020/auth/PasswordComparator.java
+++ Zaiko2020/src/jp/co/acroit/zaiko2020/auth/PasswordComparator.java
@@ -2,10 +2,8 @@ package jp.co.acroit.zaiko2020.auth;
 
 public class PasswordComparator {
 
-    //TODO: メソッド名 comparator->compare
-    //TODO: staticを取る
     // ログインフォームで入力されたパスワードを、実際のパスワードと照合する
-    public static boolean comparator(String formPass, String pass) {
+    public boolean compare(String formPass, String pass) {
         if(formPass.equals(pass)) {
             return true;
         } else {
