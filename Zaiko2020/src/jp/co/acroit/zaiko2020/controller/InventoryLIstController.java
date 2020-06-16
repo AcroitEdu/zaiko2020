891bf4a772f7eeef91f0b4b4536447a298680052
h-tajima <h-tajima@DESKTOP-06LSGLM>
2020-06-16 11:43:46 +0900

    book作成

--- Zaiko2020/src/jp/co/acroit/zaiko2020/controller/InventoryLIstController.java
+++ Zaiko2020/src/jp/co/acroit/zaiko2020/controller/InventoryLIstController.java
@@ -1,11 +1,14 @@
 package jp.co.acroit.zaiko2020.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import jp.co.acroit.zaiko2020.data.BookDataAccess;
 
 /**
  * Servlet implementation class InventoryLIstContoller
@@ -13,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/InventoryLIstContoller")
 public class InventoryLIstController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,7 +29,31 @@ public class InventoryLIstController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+		request.setCharacterEncoding("UTF-8");
+		String book = null;
+		String publisher = null;
+		String author = null;
+		String salsdate = null;
+		String stock = null;
+		String salsdateflag = null;
+		String stockflag = null;
+
+
+		book = request.getParameter("book");
+		publisher = request.getParameter("publisher");
+		author = request.getParameter("author");
+		salsdate = request.getParameter("salsdate");
+		stock = request.getParameter("stock");
+
+		String conditions = "WHERE";
+
+
+
+		//書籍検索
+		BookDataAccess bda = new BookDataAccess();
+
+
 	}
 
 }
