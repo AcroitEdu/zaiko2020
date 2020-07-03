368bdd7aec9d90b2135979d3f772c3f9cbb942dc
h-ishioka <h-ishioka@DESKTOP-GD4RHQN>
2020-07-03 10:54:22 +0900

    入荷画面表示の追加

--- Zaiko2020/src/jp/co/acroit/zaiko2020/controller/ArrivalController.java
+++ Zaiko2020/src/jp/co/acroit/zaiko2020/controller/ArrivalController.java
@@ -1,33 +1,75 @@
 package jp.co.acroit.zaiko2020.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import jp.co.acroit.zaiko2020.book.Book;
+import jp.co.acroit.zaiko2020.data.BookDataAccess;
 
 /**
- * Servlet implementation class ArrivalController
+ * 入荷画面サーブレット
+ * @version 1.0
+ * @author hiroe ishioka
  */
 @WebServlet("/ArrivalController")
 public class ArrivalController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public ArrivalController() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		//特定書籍の検索条件設定
+		int id = 0;
+
+		//書籍検索
+		BookDataAccess bda = new BookDataAccess();
+
+		HttpSession session = request.getSession();
+		session.setAttribute("error", "");
+		Book foundBook;
+
+		try {
+			//IDの取得
+			id = Integer.parseInt(request.getParameter("id"));
+
+			//IDをセッションに設定
+			session.setAttribute("id", id);
+
+			//検索
+			foundBook = bda.findid(id);
+
+			//検索結果をセッションに設定
+			session.setAttribute("book", foundBook);
+
+			//該当書籍なし ※あり得ない
+//			if (specificBook.isEmpty()) {
+//				session.setAttribute("error", "該当する書籍は見つかりませんでした。");
+//			}
+
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/ArrivalForm.jsp");
+			dispatcher.forward(request, response);
+		} catch (Exception e) {
+			session.setAttribute("error", "システムに異常が発生しています。システム管理者に連絡してください。");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/InventoryList.jsp");
+			dispatcher.forward(request, response);
+			e.printStackTrace();
+		}
+
+
+
 	}
 
 	/**
@@ -36,6 +78,11 @@ public class ArrivalController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+
+
+
+
+
 	}
 
 }
--- Zaiko2020/src/jp/co/acroit/zaiko2020/controller/ArrivalProcessingController.java
+++ Zaiko2020/src/jp/co/acroit/zaiko2020/controller/ArrivalProcessingController.java
@@ -1,11 +1,15 @@
 package jp.co.acroit.zaiko2020.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import jp.co.acroit.zaiko2020.data.BookDataAccess;
 
 /**
  * Servlet implementation class ArrivalProcessingController
@@ -13,18 +17,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ArrivalProcessingController")
 public class ArrivalProcessingController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public ArrivalProcessingController() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
@@ -36,6 +34,44 @@ public class ArrivalProcessingController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+
+		int id = 0;
+		int stock = 0;
+
+		//書籍検索
+		BookDataAccess bda = new BookDataAccess();
+
+		try {
+
+			//IDの取得
+			id = Integer.parseInt(request.getParameter("id"));
+			stock = bda.findStock(id);
+
+			//IDをセッションに設定
+			session.setAttribute("id", id);
+
+			//検索
+			foundBook = bda.findid(id);
+
+			//検索結果をセッションに設定
+			session.setAttribute("book", foundBook);
+
+			//該当書籍なし ※あり得ない
+//			if (specificBook.isEmpty()) {
+//				session.setAttribute("error", "該当する書籍は見つかりませんでした。");
+//			}
+
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/ArrivalForm.jsp");
+			dispatcher.forward(request, response);
+		} catch (Exception e) {
+			session.setAttribute("error", "システムに異常が発生しています。システム管理者に連絡してください。");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/InventoryList.jsp");
+			dispatcher.forward(request, response);
+			e.printStackTrace();
+		}
+
+
+
 	}
 
 }
