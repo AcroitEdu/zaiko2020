09613d91849e2eda56d6086f695bf6a4c19433a4
h-tajima <h-tajima@DESKTOP-06LSGLM>
2020-07-06 09:13:18 +0900

    エラーメッセージの削除

--- Zaiko2020/src/jp/co/acroit/zaiko2020/controller/UpdateResultController.java
+++ Zaiko2020/src/jp/co/acroit/zaiko2020/controller/UpdateResultController.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * 更新結果サーブレット
@@ -29,6 +30,9 @@ public class UpdateResultController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.setAttribute("error", "");
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/ResultForm.jsp");
 		dispatcher.forward(request, response);
 	}
