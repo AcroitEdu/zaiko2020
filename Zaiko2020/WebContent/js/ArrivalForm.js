8130165b90bb715533eeb84f954a96fdb19143bf
k-oishi <k-oishi@DESKTOP-GDLQN7T>
2020-07-08 14:45:53 +0900

    「1.00000005960464477999」や「1.99999994039535523」のような数字を入荷数として入力した際に、step属性によるチェックを突破される不具合の修正

--- Zaiko2020/WebContent/js/ArrivalForm.js
+++ Zaiko2020/WebContent/js/ArrivalForm.js
@@ -1,7 +1,18 @@
+//入力チェック処理
+const checkValid = () => {
+    if ($("#count").val().search(/[^0-9]/) < 0) {
+        $("#count")[0].setCustomValidity("");
+    } else {
+        var value = parseInt($("#count").val().replace(/[^0-9].*/, ""));
+        $("#count")[0].setCustomValidity(`有効な値を入力してください。有効な値として最も近いのは${value}と${value + 1}です。`);
+    }
+    return $("#inoutForm")[0].reportValidity();
+};
+
 //「実行」押下時
 $("#execute").click(function () {
-    if ($("#inoutForm")[0].reportValidity()) {
-        $("#countConfirm").text($("#count").val());
+    if (checkValid()) {
+        $("#countConfirm").text(parseInt($("#count").val()));
         $("#dialogSubmit")[0].showModal();
     }
 });
