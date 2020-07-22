a63d1f69c5d9c741fa5d43654ff752549bccf319
h-tajima <h-tajima@DESKTOP-06LSGLM>
2020-07-22 14:50:49 +0900

    追加処理作成（動作未確認）

--- Zaiko2020/src/jp/co/acroit/zaiko2020/controller/AddProcessController.java
+++ Zaiko2020/src/jp/co/acroit/zaiko2020/controller/AddProcessController.java
@@ -1,16 +1,24 @@
 package jp.co.acroit.zaiko2020.controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import jp.co.acroit.zaiko2020.book.Book;
+import jp.co.acroit.zaiko2020.data.BookDataAccess;
 
 /**
- * Servlet implementation class AddProcessController
+ * 追加処理サーブレット
+ * @version 1.0
+ * @author hiroki tajima
  */
-@WebServlet("/AddProcessController")
+@WebServlet("/AddProcess")
 public class AddProcessController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -18,16 +26,56 @@ public class AddProcessController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+//		String title = null;
+//		String publisher = null;
+//		String author = null;
+//		String isbn = null;
+//		String date = null;
+//		int price = 0;
+//		int stock = 0;
+//		int dbDeleteflg = 0;
+
+		HttpSession session = request.getSession();
+		Book book = (Book)session.getAttribute("items");
+
+
+		BookDataAccess bda = new BookDataAccess();
+		try {
+			bda.add(book);
+		} catch (SQLException e) {
+
+			session.setAttribute("error", "データべースに異常が発生しています。システム管理者に連絡してください。");
+			response.sendRedirect("/Zaiko2020/inventoryList");
+
+		} catch (Exception e) {
+			//エラーを返しリダイレクト
+			session.setAttribute("error", "システムに異常が発生しています。システム管理者に連絡してください。");
+//			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/InventoryList.jsp");
+//			dispatcher.forward(request, response);
+			response.sendRedirect("/Zaiko2020/logout");
+			e.printStackTrace();
+
+		}
+//		session.getAttribute("title");
+//		session.setAttribute("publisher", publisher);
+//		session.setAttribute("author", author);
+//		session.setAttribute("isbn", isbn);
+//		session.setAttribute("date", date);
+//		session.setAttribute("price", price);
+//		session.setAttribute("stock", stock);
+
+
+
+
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
 	}
 
 }
