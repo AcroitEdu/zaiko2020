8efa2c907eb47fe388bc871ab901c6d6c5ccf1fd
h-tajima <h-tajima@DESKTOP-06LSGLM>
2020-07-22 11:31:36 +0900

    フォワード追記

--- Zaiko2020/src/jp/co/acroit/zaiko2020/controller/AddCheckController.java
+++ Zaiko2020/src/jp/co/acroit/zaiko2020/controller/AddCheckController.java
@@ -1,14 +1,19 @@
 package jp.co.acroit.zaiko2020.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class AddCheckController
+ * 追加確認サーブレット
+ * @version 1.0
+ * @author hiroki tajima
  */
 @WebServlet("/AddCheckController")
 public class AddCheckController extends HttpServlet {
@@ -18,8 +23,37 @@ public class AddCheckController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+
+		String title = null;
+		String publisher = null;
+		String author = null;
+		String isbn = null;
+		String date = null;
+		int price = 0;
+		int stock = 0;
+		int dbDeleteflg = 0;
+
+		title = request.getParameter("bookname");
+		publisher = request.getParameter("publisher");
+		author = request.getParameter("author");
+		isbn = request.getParameter("isbn");
+		date = request.getParameter("date");
+		price = Integer.parseInt(request.getParameter("price"));
+		stock = Integer.parseInt(request.getParameter("stock"));
+
+		HttpSession session = request.getSession();
+		session.setAttribute("titile", title);
+		session.setAttribute("publisher", publisher);
+		session.setAttribute("author", author);
+		session.setAttribute("isbn", isbn);
+		session.setAttribute("date", date);
+		session.setAttribute("price", price);
+		session.setAttribute("stock", stock);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/AddCheck.jsp");
+		dispatcher.forward(request, response);
+
 	}
 
 	/**
