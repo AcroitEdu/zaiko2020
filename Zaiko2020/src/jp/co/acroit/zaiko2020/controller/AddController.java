05bc196a5e87fbf368ce177ca7866fd3ec939922
h-tajima <h-tajima@DESKTOP-06LSGLM>
2020-07-28 11:35:23 +0900

    不要行削除

--- Zaiko2020/src/jp/co/acroit/zaiko2020/controller/AddController.java
+++ Zaiko2020/src/jp/co/acroit/zaiko2020/controller/AddController.java
@@ -27,53 +27,6 @@ public class AddController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-//		String Branch = null;
-//		Branch = request.getParameter("button");
-//		System.out.println(Branch);
-//
-//
-//		String title = null;
-//		String publisher = null;
-//		String author = null;
-//		String isbn = null;
-//		LocalDate salsDate = null;
-//		int price = 0;
-//		int stock = 0;
-//		int deleteFlg = 0;
-//
-//
-//
-//
-//		switch(Branch) {
-//		case "追加":
-//			Book addbook = new Book(0, null, null, null, null, null, 0, 0, 0);
-//
-//			addbook.setName(title);
-//			addbook.setPublisher(publisher);
-//			addbook.setAuthor(author);
-//			addbook.setIsbn(isbn);
-//			addbook.setSalesDate(salsDate);
-//			addbook.setPrice(price);
-//			addbook.setStock(stock);
-//			addbook.setDeleteFlag(deleteFlg);
-//
-//			HttpSession session = request.getSession();
-//			session.setAttribute("book", addbook);
-//
-//			break;
-//		}
-//
-////		HttpSession session = request.getSession();
-////		session.setAttribute("titile", "");
-////		session.setAttribute("publisher", "");
-////		session.setAttribute("author", "");
-////		session.setAttribute("isbn", "");
-////		session.setAttribute("date", "");
-////		session.setAttribute("price", "");
-////		session.setAttribute("stock", "");
-//
-//		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/AddForm.jsp");
-//		dispatcher.forward(request, response);
 	}
 
 	/**
